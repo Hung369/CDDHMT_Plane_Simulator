c33d09518bd4e45e4a4afe8fd6c1e60e49f8e4d3
Hung369 <nguyenmhung2310@gmail.com>
2023-12-14 20:09:10 +0700

    update icon

--- src/App.jsx
+++ src/App.jsx
@@ -22,17 +22,15 @@ function Cube({ position, size, color }) {
 const App = () => {
   return (
     <>
-      <Canvas>
-        <directionalLight position={[0, 0, 4]} intensity={0.8} />
-        <ambientLight intensity={0.5} />
-        <group position={[0, -1, 0]}> {/* gather all elements into a group, then we can setup position for all elements */}
-          <Cube position={[1, 0, 0]} color={"green"} size={[1, 1, 1]} />
-          <Cube position={[-1, 0, 0]} color={"hotpink"} size={[1, 1, 1]} />
-          <Cube position={[-1, 2, 0]} color={"blue"} size={[1, 1, 1]} />
-          <Cube position={[1, 2, 0]} color={"yellow"} size={[1, 1, 1]} />
-        </group>
-        <OrbitControls/>
-      </Canvas>
+      <directionalLight position={[0, 0, 4]} intensity={0.8} />
+      <ambientLight intensity={0.5} />
+      <group position={[0, -1, 0]}> {/* gather all elements into a group, then we can setup position for all elements */}
+        <Cube position={[1, 0, 0]} color={"green"} size={[1, 1, 1]} />
+        <Cube position={[-1, 0, 0]} color={"hotpink"} size={[1, 1, 1]} />
+        <Cube position={[-1, 2, 0]} color={"blue"} size={[1, 1, 1]} />
+        <Cube position={[1, 2, 0]} color={"yellow"} size={[1, 1, 1]} />
+      </group>
+      <OrbitControls/>
     </>
   )
 }
