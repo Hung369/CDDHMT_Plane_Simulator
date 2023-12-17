dcc21554b036ced9f595a0e3272e1d2e4c0cccad
Hung369 <nguyenmhung2310@gmail.com>
2023-12-17 11:05:28 +0700

    complete the gameplay, need more justify

--- src/Gameplay.js
+++ src/Gameplay.js
@@ -4,18 +4,21 @@ import { F16 } from './Airplane';
 import { sky_showroom, base_showroom } from './Texture_Loader';
 import { createDirectionalLight } from './LightSource';
 import { plane_camera } from './Cam';
+import { updatePlaneAxis } from './Controller'
 
 let camera, scene, renderer;
-let cameraControls, lightSource;
+let lightSource;
 let jet_fighter;
-const camplanePosition = new THREE.Vector3(0,3,7);
-const x = new THREE.Vector3(1,0,0);
-const y = new THREE.Vector3(0,1,0);
-const z = new THREE.Vector3(0,0,1);
+export const planePosition = new THREE.Vector3(0, 3, 7);
+
+const x = new THREE.Vector3(1, 0, 0);
+const y = new THREE.Vector3(0, 1, 0);
+const z = new THREE.Vector3(0, 0, 1);
+
 const delayedRotMatrix = new THREE.Matrix4();
 const delayedQuaternion = new THREE.Quaternion();
 
-function init(){
+function GameScene() {
     scene = new THREE.Scene();
     let skyscene = sky_showroom();
     scene.background = skyscene;
@@ -27,7 +30,7 @@ function init(){
     document.body.appendChild(renderer.domElement);
 
     // ground
-    let groundGeometry = new THREE.BoxGeometry(180, 0.01, 180);
+    let groundGeometry = new THREE.BoxGeometry(580, 0.01, 580);
     let groundSurface = base_showroom();
     let groundMaterial = new THREE.MeshPhongMaterial({ map: groundSurface });
     let ground = new THREE.Mesh(groundGeometry, groundMaterial);
@@ -37,13 +40,13 @@ function init(){
     // jet
     jet_fighter = F16();
     scene.add(jet_fighter);
-    
+
     // camera
-    camera = plane_camera(scene, camplanePosition);
+    camera = plane_camera(scene, planePosition);
     scene.add(camera)
 
-    // controller cam
-    cameraControls = new OrbitControls(camera, renderer.domElement);
+    // // controller cam
+    // cameraControls = new OrbitControls(camera, renderer.domElement);
 
     // lightsource
     lightSource = createDirectionalLight(0xF4E99B, 5.0);
@@ -55,15 +58,46 @@ function init(){
     scene.add(axesHelper);
 }
 
-function animate(){
+function animate() {
     requestAnimationFrame(animate);
-    cameraControls.update();
+    // cameraControls.update();
     render()
 }
 
-function render(){
+function render() {
+    updatePlaneAxis(x, y, z, planePosition, camera);
+    const rotMatrix = new THREE.Matrix4().makeBasis(x, y, z);
+    const matrix = new THREE.Matrix4().multiply(
+        new THREE.Matrix4().makeTranslation(planePosition.x, planePosition.y, planePosition.z)
+    ).multiply(rotMatrix);
+
+    jet_fighter.matrixAutoUpdate = false;
+    jet_fighter.matrix.copy(matrix);
+
+    var quaternionA = new THREE.Quaternion().copy(delayedQuaternion);
+    var quaternionB = new THREE.Quaternion();
+    quaternionB.setFromRotationMatrix(rotMatrix);
+
+    var interpolationFactor = 0.175;
+    var interpolatedQuaternion = new THREE.Quaternion().copy(quaternionA);
+    interpolatedQuaternion.slerp(quaternionB, interpolationFactor);
+    delayedQuaternion.copy(interpolatedQuaternion);
+
+    delayedRotMatrix.identity();
+    delayedRotMatrix.makeRotationFromQuaternion(delayedQuaternion);
+
+    const cameraMatrix = new THREE.Matrix4().multiply(
+        new THREE.Matrix4().makeTranslation(planePosition.x, planePosition.y, planePosition.z))
+        .multiply(delayedRotMatrix).multiply(new THREE.Matrix4().makeRotationX(-0.2))
+        .multiply(new THREE.Matrix4().makeTranslation(0, 8, 8)
+        );
+
+    camera.matrixAutoUpdate = false;
+    camera.matrix.copy(cameraMatrix);
+    camera.matrixWorldNeedsUpdate = true;
+
     renderer.render(scene, camera);
 }
 
-init();
+GameScene();
 animate();
