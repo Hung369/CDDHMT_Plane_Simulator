9db4f433fe5f70e082af8141342985ca2fe1b74a
Nguyen Manh Hung <81510821+Hung369@users.noreply.github.com>
2023-12-19 09:00:15 +0700

    update function loading airplane models

--- src/Airplane.js
+++ src/Airplane.js
@@ -23,8 +23,51 @@ export function F16() {
   });
 
   jet.position.set(planePosition.x, planePosition.y, planePosition.z);
-  // jet.matrixAutoUpdate = false;
-  // jet.matrixWorldNeedsUpdate = true;
+  return jet;
+}
 
+export function Boeing() {
+  var boeing = new THREE.Object3D();
+  const loader = new GLTFLoader();
+
+  loader.load('./src/model/boeing_757.glb', (gltf) => {
+    gltf.scene.traverse(function (node) {
+      if (node.isMesh) {
+        // Enable shadow casting
+        node.castShadow = true;
+
+        // Swap the material
+        node.material = new THREE.MeshPhongMaterial({ map: node.material.map });
+
+      }
+    });
+    gltf.scene.scale.set(0.8, 0.8, 0.8);
+    boeing.add(gltf.scene);
+  });
+
+  boeing.position.set(planePosition.x, planePosition.y, planePosition.z);
+  return boeing;
+}
+
+export function X_wing() {
+  var jet = new THREE.Object3D();
+  const loader = new GLTFLoader();
+
+  loader.load('./src/model/X_wing.glb', (gltf) => {
+    gltf.scene.traverse(function (node) {
+      if (node.isMesh) {
+        // Enable shadow casting
+        node.castShadow = true;
+
+        // Swap the material
+        node.material = new THREE.MeshPhongMaterial({ map: node.material.map });
+
+      }
+    });
+    gltf.scene.scale.set(0.8, 0.8, 0.8);
+    jet.add(gltf.scene);
+  });
+
+  jet.position.set(planePosition.x, planePosition.y, planePosition.z);
   return jet;
 }
